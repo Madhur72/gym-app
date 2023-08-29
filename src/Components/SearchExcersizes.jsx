a10d147507a56be491d2d1dfa7b0c37aa40686e2
Madhur72 <madhurdubey72@gmail.com>
2023-08-29 11:56:24 +0530

    added search bar

--- src/Components/SearchExcersizes.jsx
+++ src/Components/SearchExcersizes.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Box, Typography, TextField, Stack,Button } from "@mui/material";
+import React from "react";
 
 const SearchExcersizes = () => {
   return (
-    <div>
-      <p>SearchExcersizes</p>
-    </div>
-  )
-}
+    <Box>
+      <Typography align="center" mt={7} variant="h4" fontWeight={700}>
+        Awesome Excersizes You <br /> Should Know
+      </Typography>
+      <Stack direction='row' spacing={0.5}justifyContent='center' mt={3}>
+        <TextField id="outlined-basic" label="Search" variant="outlined" sx={{width:"400px"}} />
+        <Button variant="contained" color="error">
+          Search
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
 
-export default SearchExcersizes
+export default SearchExcersizes;

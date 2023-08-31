bc93b41456520791dd8dd9c4f7d9070e8e0d0b44
Madhur72 <madhurdubey72@gmail.com>
2023-08-31 13:58:33 +0530

    Errors fixed

--- src/Components/BodyPart.jsx
+++ src/Components/BodyPart.jsx
@@ -1,10 +1,14 @@
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
-
-const BodyPart = () => {
+import logo from "../assets/gym-logo.svg"
+const BodyPart = ({item,bodyPart,setBodyPart}) => {
   return (
-    <div>
-      
-    </div>
+    <Stack>
+      <img src={logo} alt="" width="50px" height="50px" />
+      <Typography>
+        {item}
+      </Typography>
+    </Stack>
   )
 }
 
--- src/Components/HorizontalScroll.jsx
+++ src/Components/HorizontalScroll.jsx
@@ -1,11 +1,17 @@
 import { Box } from "@mui/material";
 import React from "react";
 import BodyPart from "./BodyPart";
-const HorizontalScroll = ({ bodyParts,setBodyParts }) => {
+const HorizontalScroll = ({ bodyParts, setBodyPart, bodyPart }) => {
   return (
     <div>
       {bodyParts.map((item) => (
-        <Box key={item.id||item}><BodyPart item = {item} bodyParts = {bodyParts} setBodyParts = {setBodyParts}/></Box>
+        <Box key={item.id || item}>
+          <BodyPart
+            item={item}
+            bodyPart={bodyPart}
+            setBodyPart={setBodyPart}
+          />
+        </Box>
       ))}
     </div>
   );
